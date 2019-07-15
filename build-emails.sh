b7ffc1c5fe403a5e73b465841ddcbb05b94f0222
ash <ash@strategicdata.com.au>
2019-07-15 17:05:07 +1000

    Make build emails script configurable via env

--- build-emails.sh
+++ build-emails.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-emaildir=/emails/
-emailfiles="$emaildir/*.mjml"
+EMAIL_DIR=/emails/
+IN_EXT="${IN_EXT:-mjml}"
+EMAIL_FILES="$EMAIL_DIR/*.$IN_EXT"
+OUT_EXT="${OUT_EXT:-tt2}"
 
-for file in $emailfiles
+for file in $EMAIL_FILES
 do
-  mjml -r "$file" -o $(echo "$file" | sed 's/mjml$/tt2/')
+  mjml -r "$file" -o $(echo "$file" | sed "s/${IN_EXT}$/$OUT_EXT/")
 done
